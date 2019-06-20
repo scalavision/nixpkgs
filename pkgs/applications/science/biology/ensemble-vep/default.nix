5058813a066b705779b26c8b99e47e4fa2e2de16
scalavision <scalavision@gmail.com>
2019-06-20 14:06:40 +0200

    troubleshooting usage of curl to download all dependencies

--- pkgs/applications/science/biology/ensemble-vep/default.nix
+++ pkgs/applications/science/biology/ensemble-vep/default.nix
@@ -1,39 +1,86 @@
-{ stdenv, fetchFromGitHub, makeWrapper, zlib, perl, perlPackages }:
+{ stdenv, fetchFromGitHub, makeWrapper, zlib, perl, perlPackages, curl, which, unzip }:
 
-stdenv.mkDerivation rec {
-
+let 
     version = "96.3";
-    name = "ensembl-vep";
 
-    # TODO: Need to download all dependencies in an outer src block
-    # TODO: Then you can use that downloaded stuff to actually do the installation
-    
     src = fetchFromGitHub {
         owner = "Ensembl";
-        repo = name;
+        repo = "ensemple-vep";
         rev = "release/${version}";
         sha256 = "0nm1227f025f5wdi58ny2nxhjfrl0n2zq2qys91p96bxszwcc935";
     };
 
-    propagatedBuildInputs = [ perl perlPackages.DBI ];
+    deps = stdenv.mkDerivation {
+        
+        name = "ensembl-vep-${version}-dep";
 
-    nativeBuildInputs = [ zlib makeWrapper ];
+        inherit src;
+
+        propagatedBuildInputs = [ perl perlPackages.DBI perlPackages.DBDmysql perlPackages.HTTPTinyish curl which unzip ];
+        
+        buildInput = [ curl ];
+
+        nativeBuildInputs = [ zlib makeWrapper curl ];
+
+        buildPhase = ''
+            echo "skipping build Phase"
+        '';
+
+        patchPhase = ''
+            echo "patching INSTALL.pl"
+
+            cat INSTALL.pl | grep curl  
+            
+            substituteAllInPlace INSTALL.pl \
+              --replace "curl" ${curl}/bin/curl \
+              --replace "$CAN_USE_CURL      = 1 if `which curl` =~ /\/curl/;" "$CAN_USE_CURL      = 1"
+
+            cat INSTALL.pl | grep curl  
+
+            echo "Was it patched"
+
+        '';
+
+        installPhase = ''
+            echo "Running perl install script"
+
+            export PATH=$PATH/${curl}/bin
+
+            echo "TESTING CURL"
+            curl --help
+
+            echo "ANYTING?"
+
+            ls -hal ${curl}/bin
+
+            perl INSTALL.pl
+        '';
+    };
+
+in
+stdenv.mkDerivation rec {
+    
+    name = "ensemple-vep-${version}";
+
+    inherit src;
 
     makeFlags = [ "PREFIX=$(out)/bin" ];
-
+    
     preInstall = "mkdir -p $out/bin";
 
-    buildPhase = ''
-        echo "skipping build Phase"
-    '';
+    propagatedBuildInputs = [ perl perlPackages.DBI ];
+    
+    nativeBuildInputs = [ zlib makeWrapper ];
 
-    installPhase = ''
-        
-        perl INSTALL.pl
+    buildPhase = ''
+        mkdir -p $out/bin
+        mkdir -p $out/lib
+        cp -R ${deps} $out/lib
     '';
 
     postFixup = ''
-        wrapProgram $out/bin/vep --prefix PERL5LIB : $PERL5LIB
+        wrapProgram $out/bin/vep --prefix PERL5LIB : $PERL5LIB --prefix VEPLIB: $out/lib
     '';
+
 
 }
