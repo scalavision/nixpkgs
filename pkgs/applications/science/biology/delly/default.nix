b8b1623eb9430004f91470a6bd1f02c83ab04919
scalavisison <scalavision@gmail.com>
2019-07-25 17:12:22 +0200

    refactoring: reuse pname in git url

--- pkgs/applications/science/biology/delly/default.nix
+++ pkgs/applications/science/biology/delly/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
     version = "0.8.1";
 
     src = fetchgit {
-        url = "https://github.com/dellytools/delly.git";
+        url = "https://github.com/dellytools/${pname}.git";
         rev = "1da97218e5324c00acb2bb33b0e7985fad4d8079";
         sha256 = "0ip6x1g5dr7j8w3qhvg4d1p4ix88mcx0jjb01zsnaaxq3gbh3b09";
     };
