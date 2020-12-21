6b8903df07850d81f9873b361126f2477356fdc6
scalavision <scalavision@gmail.com>
2020-12-21 13:25:57 +0100

    ms-python.vscode-pylance: 2020.11.2 -> 2020.12.2

--- pkgs/misc/vscode-extensions/default.nix
+++ pkgs/misc/vscode-extensions/default.nix
@@ -40,8 +40,8 @@ let
         mktplcRef = {
           name = "vscode-pylance";
           publisher = "MS-python";
-          version = "2020.11.2";
-          sha256 = "0n2dm21vgzir3hx1m3pmx7jq4zy3hdxfsandd2wv5da4fs9b5g50";
+          version = "2020.12.2";
+          sha256 = "0n2dm21vgzir3hx1m3pmx7jq4zy3hdxfsandd2wv5da4fs9b5g51";
         };
 
         buildInputs = [ nodePackages.pyright ];
